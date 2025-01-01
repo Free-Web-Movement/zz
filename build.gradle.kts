1026e354155f96a6447c77d154e1cb682a46ec30
calidion <calidion@gmail.com>
2025-01-01 21:56:26 +0800

    updated to kotlin 2.0+

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
+    alias(libs.plugins.compose.compiler) apply false
+
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29"
     id("org.jetbrains.kotlinx.kover") version "0.5.0"
 }
