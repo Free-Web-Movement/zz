8f01ecd688beab3f5f3ca57ed53647914418116f
calidion <calidion@gmail.com>
2025-01-20 01:17:53 +0800

    added input check for peer addition

---
+++ app/src/main/java/io/github/freewebmovement/zz/system/IsValid.kt
@@ -0,0 +1,23 @@
+package io.github.freewebmovement.zz.system
+
+import android.net.InetAddresses
+import android.os.Build
+import android.util.Patterns
+
+class IsValid {
+    companion object {
+        fun IP(ip: String): Boolean {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                InetAddresses.isNumericAddress(ip)
+            } else {
+                Patterns.IP_ADDRESS.matcher(ip).matches()
+            }
+        }
+
+        fun port(port: Int): Boolean {
+            val min = (1 shl 10) + 1
+            val max = (1 shl 16) - 1
+            return port in min..max
+        }
+    }
+}
--- app/src/main/java/io/github/freewebmovement/zz/ui/content/peer/AddPeer.kt
+++ app/src/main/java/io/github/freewebmovement/zz/ui/content/peer/AddPeer.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,10 +20,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.freewebmovement.zz.R
+import io.github.freewebmovement.zz.system.IsValid
 import io.github.freewebmovement.zz.ui.common.PageType
 
 
@@ -29,6 +34,8 @@ fun AddPeer(updatePage: (value: PageType) -> Unit) {
     val scrollState = rememberScrollState()
     var ip by remember { mutableStateOf(TextFieldValue("")) }
     var port by remember { mutableStateOf(TextFieldValue("")) }
+    val openIPDialog = remember { mutableStateOf(false) }
+    val openPortDialog = remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -37,7 +44,6 @@ fun AddPeer(updatePage: (value: PageType) -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         OutlinedTextField(
-//            modifier = Modifier.fillMaxWidth(),
             value = ip,
             onValueChange = { newText ->
                 ip = newText
@@ -46,11 +52,11 @@ fun AddPeer(updatePage: (value: PageType) -> Unit) {
             placeholder = { Text(text = stringResource(R.string.please_input_your_ip_here)) },
         )
         OutlinedTextField(
-//            modifier = Modifier.fillMaxWidth(),
             value = port,
             onValueChange = { newText ->
                 port = newText
             },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             label = { Text(text = stringResource(R.string.please_input_your_port_here)) },
             placeholder = { Text(text = stringResource(R.string.please_input_your_port_here)) },
         )
@@ -58,6 +64,21 @@ fun AddPeer(updatePage: (value: PageType) -> Unit) {
         Button(
             modifier = Modifier.padding(top = 16.dp),
             onClick = {
+                if (!IsValid.IP(ip.text)) {
+                    openIPDialog.value = true
+                    return@Button
+                }
+                try {
+                    val portNumber = port.text.toInt()
+                    if (!IsValid.port(portNumber)) {
+                        openPortDialog.value = true
+                        return@Button
+                    }
+                } catch (e: Exception) {
+                    openPortDialog.value = true
+                    return@Button
+                }
+
             }
         ) {
             Text(
@@ -65,12 +86,47 @@ fun AddPeer(updatePage: (value: PageType) -> Unit) {
             )
         }
     }
+
+    if (openIPDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onDismissRequest.
+                openIPDialog.value = false
+            },
+            title = { Text(text = stringResource(R.string.invalid_ip)) },
+            text = { Text(text = stringResource(R.string.please_input_valid_ip)) },
+            confirmButton = {
+                TextButton(onClick = {
+                    openIPDialog.value = false
+                }) { Text(stringResource(R.string.ok)) }
+            }
+        )
+    }
+    if (openPortDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onDismissRequest.
+                openPortDialog.value = false
+            },
+            title = { Text(text = stringResource(R.string.invalid_port)) },
+            text = { Text(text = stringResource(R.string.please_input_valid_port)) },
+            confirmButton = {
+                TextButton(onClick = {
+                    openPortDialog.value = false
+                }) { Text(stringResource(R.string.ok)) }
+            }
+        )
+    }
 }
 
 
 @Preview
 @Composable
 private fun Preview() {
-    AddPeer() {
+    AddPeer {
     }
 }
